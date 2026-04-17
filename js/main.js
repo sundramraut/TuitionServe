be7ffbbffb77594303f14c97e6ed26e4533f2c20
Sundram Raut <101653315+sundramraut@users.noreply.github.com>
2026-04-17 07:13:23 +0545

    Add menu toggle and messenger open functions

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+}
+
+function openMessenger() {
+  alert("Messenger will open (we will build later)");
+}
